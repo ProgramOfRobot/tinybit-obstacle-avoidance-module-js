50bf0262865438c9c8dbc8f660d9308bb683966c
Helloworldzz <58582177+ProgramOfRobot@users.noreply.github.com>
2022-08-13 14:19:23 +0800

    Writed some packing notes. Waiting for the detailed notes.

--- main.ts
+++ main.ts
@@ -1,9 +1,38 @@
+/**
+ * A module that can help you control Tiny-bit better.
+ * @author: 9reyyy
+ * @version: a1.0.0
+ */
 namespace ObstacleAvoidance {
+
+    /**
+     * A namespace that saves all the data about Tiny-bit.
+     */
     export namespace Data {
+
+        /**
+         * A variable that records if the car is running.
+         */
         export let isRunning  = false;
+
+        /**
+         * A variable that records if the car is stopping.
+         */
         export let isStopped  = false;
+
+        /**
+         * A variable that records if the car is running fast.
+         */
         export let isFastMode = true;
+
+        /**
+         * A variable that uses to delay the time between turning and running.
+         */
         export let delayTime  = 350;
+
+        /**
+         * An object to save the checking distance of ultrasonic.
+         */
         export const ultrasonic = {
             FAST: {
                 distance: 30
@@ -12,6 +41,10 @@ namespace ObstacleAvoidance {
                 distance: 24
             }
         };
+
+        /**
+         * An object to save the running speed for Tiny-bit.
+         */
         export const running = {
             FAST: {
                 runningSpeed: 225
@@ -20,6 +53,10 @@ namespace ObstacleAvoidance {
                 runningSpeed: 105
             }
         };
+
+        /**
+         * An object to save the turning speed and time for Tiny-bit.
+         */
         export const turning = {
             FAST: {
                 turningSpeed: 170, // 170
@@ -31,6 +68,10 @@ namespace ObstacleAvoidance {
             }
         };
 
+        /**
+         * Returns the checking distance of ultrasonic.
+         * @returns object
+         */
         export function getUltrasonic() {
             if (isFastMode) {
                 return ultrasonic.FAST;
@@ -39,6 +80,10 @@ namespace ObstacleAvoidance {
             }
         }
 
+        /**
+         * Returns the running speed of Tiny-bit.
+         * @returns object
+         */
         export function getRunning() {
             if (isFastMode) {
                 return running.FAST;
@@ -47,6 +92,10 @@ namespace ObstacleAvoidance {
             }
         }
 
+        /**
+         * Returns the turning speed and the time of Tiny-bit.
+         * @returns object
+         */
         export function getTurning() {
             // if (isFastMode) {
             //     return turning.FAST;
@@ -56,50 +105,92 @@ namespace ObstacleAvoidance {
             return turning.FAST;
         }
 
-        function changeFastMode() {
+        /**
+         * Switch 'isFastMode' to the opposite state.
+         * @returns void
+         */
+        function switchFastMode() {
             isFastMode = !isFastMode;
         }
 
-        function changeRunning() {
+        /**
+         * Switch 'isRunning' to the opposite state.
+         * @returns void
+         */
+        function switchRunning() {
             isRunning = !isRunning;
         }
     };
 
+    /**
+     * A namespace that can help you control Tiny-bit.
+     */
     export namespace CarControlling {
-        function spinTurn(direction: Tinybit.CarState, times: number = 1) {
+        /**
+         * Let Tiny-bit turns to a direction.
+         * @param direction The direction that you want to turn.
+         * @param frequency The frequency that you want to turn.
+         * @returns void
+         */
+        function spinTurn(direction: Tinybit.CarState, frequency: number = 1) {
             carStop();
-            while (times > 0) {
+            while (frequency > 0) {
                 const turning = Data.getTurning()
                 Tinybit.CarCtrlSpeed(direction, turning.turningSpeed);
                 basic.pause(turning.turningTime);
                 Tinybit.CarCtrl(Tinybit.CarState.Car_Stop);
                 basic.pause(Data.delayTime);
-                times--;
+                frequency--;
             }
         }
         
+        /**
+         * Let Tiny-bit go forward.
+         * @returns void
+         */
         export function carForward() {
             Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, Data.getRunning().runningSpeed);
         }
 
+        /**
+         * Let Tiny-bit go backward.
+         * @returns void
+         */
         export function carBackward() {
             Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, Data.getRunning().runningSpeed);
         }
 
+        /**
+         * Let Tiny-bit turn left.
+         * @returns void
+         */
         export function carTurnLeft(times: number = 1) {
             spinTurn(Tinybit.CarState.Car_SpinLeft, times);
         }
 
+        /**
+         * Let Tiny-bit turn right.
+         * @returns void
+         */
         export function carTurnRight(times: number = 1) {
             spinTurn(Tinybit.CarState.Car_SpinRight, times);
         }
 
+        /**
+         * Let Tiny-bit stop.
+         * @returns void
+         */
         export function carStop() {
             Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 30);
             basic.pause(100);
             Tinybit.CarCtrl(Tinybit.CarState.Car_Stop);
         }
 
+        /**
+         * Checks if there's any obstacle in front of Tiny-bit.
+         * @param ifTrue if it's true then do sth.
+         * @returns boolean
+         */
         export function ifAnyObstacleFronted(ifTrue: () => void): boolean {
             if (Tinybit.Ultrasonic_Car() <= Data.getUltrasonic().distance) {
                 ifTrue();
@@ -110,10 +201,18 @@ namespace ObstacleAvoidance {
         }
     }
 
+    /**
+     * Initializes this module.
+     * @returns void
+     */
     export function initialize() {
         
     }
 
+    /**
+     * Do the things like checking the obstacles, going forward or anything.
+     * @returns void
+     */
     export function runEachLoop() {
         CarControlling.ifAnyObstacleFronted(() => {
             CarControlling.carStop();
@@ -128,7 +227,3 @@ namespace ObstacleAvoidance {
         CarControlling.carForward();
     }
 };
-
-basic.forever(() => {
-    ObstacleAvoidance.runEachLoop();
-})
